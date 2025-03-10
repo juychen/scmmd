e5689a84680bacdfd5c36d5572be9087cd52f11f
junyichen <winghare@hotmail.com>
2025-03-10 17:29:44 +0800

    Add pseudo cells in theintegration

--- harmony.py
+++ harmony.py
@@ -8,7 +8,10 @@ from harmonypy import run_harmony
 import pandas as pd
 import os
 import scanpy.external as sce
-adata_concat = anndata.read_h5ad('output/merged-PFC-concat-clustering.h5ad')
+
+input_file = 'atacsc-3region-pseudo-clustering.h5ad'
+
+adata_concat = anndata.read_h5ad(f'output/{input_file}')
 
 default_n_threads = 1
 os.environ['OPENBLAS_NUM_THREADS'] = f"{default_n_threads}"
@@ -21,17 +24,25 @@ os.environ['OMP_NUM_THREADS'] = f"{default_n_threads}"
 #                  random_state=0,
 #                  nclust=100,
 #                  max_iter_harmony=20)
-sce.pp.harmony_integrate(adata_concat, 'batch')
+sce.pp.harmony_integrate(adata_concat, 'batch',
+                         **{'nclust':9,'lamb':1,'epsilon_harmony':0,'epsilon_cluster':0,'max_iter_harmony':10})
 #adata_concat.obsm['X_pca'] = ho.Z_corr.T
-adata_concat.obs['umap_0'] = adata_concat.obsm['X_umap'][:, 0]
-adata_concat.obs['umap_1'] = adata_concat.obsm['X_umap'][:, 1]
-fig, axes = plt.subplots(figsize=(8, 3), dpi=250, ncols=2)
+# adata_concat.obs['umap_0'] = adata_concat.obsm['X_umap'][:, 0]
+# adata_concat.obs['umap_1'] = adata_concat.obsm['X_umap'][:, 1]
+# 
 
 sc.pp.neighbors(adata_concat, n_neighbors=20,use_rep='X_pca_harmony')
 sc.tl.leiden(adata_concat, resolution=1.5)
 sc.tl.umap(adata_concat)
 
-adata_concat.write_h5ad('output/merged-PFC-harmony-clustering.h5ad')
+input_name = input_file.split('.h5ad')[0]
+
+adata_concat.write_h5ad(f'output/{input_name}.harmony.h5ad')
+
+adata_concat.obs['umap_0'] = adata_concat.obsm['X_umap'][:, 0]
+adata_concat.obs['umap_1'] = adata_concat.obsm['X_umap'][:, 1]
+# 
+fig, axes = plt.subplots(figsize=(8, 3), dpi=250, ncols=2)
 ax = axes[0]
 categorical_scatter(ax=ax,
                     data=adata_concat,
@@ -43,9 +54,9 @@ categorical_scatter(ax=ax,
 ax = axes[1]
 categorical_scatter(ax=ax,
                     data=adata_concat,
-                    hue='pred_Class',
+                    hue='celltype.L1',
                     show_legend=True,
                     max_points=None,
                     s=1)
 
-plt.savefig('output/merged-PFC-concat-clustering.pdf')
+plt.savefig(f'output/{input_name}.pdf')
