f3dc5145c13c3236d38ea4ccb965723cb70c9e76
junyichen <winghare@hotmail.com>
2025-04-09 11:01:02 +0800

    Edit pycistarget

--- merge_allc.sh
+++ merge_allc.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+source /home/junyichen/anaconda3/etc/profile.d/conda.sh
+conda activate allcools
+
 # allcools merge-allc --chrom_size_path /home/junyichen/code/whole_mouse_brain/wmb/files/mm10.main.chrom.sizes --allc_paths /data2st1/junyi/single_allcs/PFC/Vascular/*gz --output_path /data2st1/junyi/merged_allcs/PFC/Vascular.tsv.gz
 # allcools merge-allc --chrom_size_path /home/junyichen/code/whole_mouse_brain/wmb/files/mm10.main.chrom.sizes --allc_paths /data2st1/junyi/single_allcs/PFC/OPC-Oligo/*gz --output_path /data2st1/junyi/merged_allcs/PFC/OPC-Oligo.tsv.gz
 # allcools merge-allc --chrom_size_path /home/junyichen/code/whole_mouse_brain/wmb/files/mm10.main.chrom.sizes --allc_paths /data2st1/junyi/single_allcs/PFC/Astro-Epen/*gz --output_path /data2st1/junyi/merged_allcs/PFC/Astro-Epen.tsv.gz
@@ -14,10 +17,10 @@
 # allcools merge-allc --chrom_size_path /home/junyichen/code/whole_mouse_brain/wmb/files/mm10.main.chrom.sizes --allc_paths /data2st1/junyi/single_allcs/HIP/Vascular/*gz --output_path /data2st1/junyi/merged_allcs/HIP/Vascular.tsv.gz
 # allcools merge-allc --chrom_size_path /home/junyichen/code/whole_mouse_brain/wmb/files/mm10.main.chrom.sizes --allc_paths /data2st1/junyi/single_allcs/HIP/OPC-Oligo/*gz --output_path /data2st1/junyi/merged_allcs/HIP/OPC-Oligo.tsv.gz
 # allcools merge-allc --chrom_size_path /home/junyichen/code/whole_mouse_brain/wmb/files/mm10.main.chrom.sizes --allc_paths /data2st1/junyi/single_allcs/HIP/Astro-Epen/*gz --output_path /data2st1/junyi/merged_allcs/HIP/Astro-Epen.tsv.gz
-source /home/junyichen/anaconda3/etc/profile.d/conda.sh
-
-conda activate allcools
-cd /data2st1/junyi/single_allcs/HIP/Neuron
-allcools merge-allc --chrom_size_path /home/junyichen/code/whole_mouse_brain/wmb/files/mm10.main.chrom.sizes --allc_paths /data2st1/junyi/single_allcs/HIP/Neuron/hip_neuron.txt --output_path /data2st1/junyi/merged_allcs/HIP/Neuron.tsv.gz
+# cd /data2st1/junyi/single_allcs/HIP/Neuron
+# allcools merge-allc --chrom_size_path /home/junyichen/code/whole_mouse_brain/wmb/files/mm10.main.chrom.sizes --allc_paths /data2st1/junyi/single_allcs/HIP/Neuron/hip_neuron.txt --output_path /data2st1/junyi/merged_allcs/HIP/Neuron.tsv.gz
 # allcools merge-allc --chrom_size_path /home/junyichen/code/whole_mouse_brain/wmb/files/mm10.main.chrom.sizes --allc_paths /data2st1/junyi/single_allcs/HIP/Immune/*gz --output_path /data2st1/junyi/merged_allcs/HIP/Immune.tsv.gz
 
+# allcools merge-allc --chrom_size_path /home/junyichen/code/whole_mouse_brain/wmb/files/mm10.main.chrom.sizes --allc_paths /data2st1/junyi/single_allcs/HIP/HIP.txt --output_path /data2st1/junyi/merged_allcs/HIP/Immune.tsv.gz
+allcools merge-allc --chrom_size_path /home/junyichen/code/whole_mouse_brain/wmb/files/mm10.main.chrom.sizes --allc_paths /data2st1/junyi/single_allcs/AMY/AMY.txt --output_path /data2st1/junyi/merged_allcs/AMY.tsv.gz
+allcools merge-allc --chrom_size_path /home/junyichen/code/whole_mouse_brain/wmb/files/mm10.main.chrom.sizes --allc_paths /data2st1/junyi/single_allcs/PFC/PFC.txt --output_path /data2st1/junyi/merged_allcs/PFC.tsv.gz
