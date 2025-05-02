579235e95551056b50348b09501f9f6b2efd1f81
junyichen <winghare@hotmail.com>
2025-05-02 15:55:41 +0800

    Add memnto test

--- 11dar.sh
+++ 11dar.sh
@@ -5,14 +5,17 @@ conda activate snapatac2
 python 11darbysubset.py --input /data2st1/junyi/output/atac0416/3REGIONS_peak.h5ads \
 --celltype_column region_nt \
 --output /data2st1/junyi/output/atac0416/dar \
+--method memento-binary \
 --region HIP
 
 python 11darbysubset.py --input /data2st1/junyi/output/atac0416/3REGIONS_peak.h5ads \
 --celltype_column region_nt \
 --output /data2st1/junyi/output/atac0416/dar \
+--method memento-binary \
 --region AMY
 
 python 11darbysubset.py --input /data2st1/junyi/output/atac0416/3REGIONS_peak.h5ads \
 --output /data2st1/junyi/output/atac0416/dar \
 --celltype_column region_nt \
+--method memento-binary \
 --region PFC
