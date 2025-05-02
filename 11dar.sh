6bc094b1aad2500f124c8e2e69c130df588d8fda
junyichen <winghare@hotmail.com>
2025-05-02 09:51:24 +0800

    edit submit 11dar.sh

--- 11dar.sh
+++ 11dar.sh
@@ -3,13 +3,16 @@ source /home/junyichen/anaconda3/etc/profile.d/conda.sh
 
 conda activate snapatac2
 python 11darbysubset.py --input /data2st1/junyi/output/atac0416/3REGIONS_peak.h5ads \
+--celltype_column region_nt \
 --output /data2st1/junyi/output/atac0416/dar \
 --region HIP
 
 python 11darbysubset.py --input /data2st1/junyi/output/atac0416/3REGIONS_peak.h5ads \
+--celltype_column region_nt \
 --output /data2st1/junyi/output/atac0416/dar \
 --region AMY
 
 python 11darbysubset.py --input /data2st1/junyi/output/atac0416/3REGIONS_peak.h5ads \
 --output /data2st1/junyi/output/atac0416/dar \
+--celltype_column region_nt \
 --region PFC
