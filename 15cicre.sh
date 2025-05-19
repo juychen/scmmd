de7d9af79e4592462eebef26398d98b6cfe26815
Junyi <winghare@hotmail.com>
2025-05-19 16:29:14 +0800

    EDIT THE ccre

--- 15cicre.sh
+++ 15cicre.sh
@@ -4,36 +4,36 @@ conda activate snapatac2
 
 python 15circepipeline.py --input output/atac0416/3REGIONS_peak.h5ads \
 --celltype_column region_nt \
---output /data2st1/junyi/output/atac0416/cicre/region_nt \
+--output output/atac0416/cicre/region_nt \
 --region HIP \
 --condition MC 
 
 python 15circepipeline.py --input output/atac0416/3REGIONS_peak.h5ads \
 --celltype_column region_nt \
---output /data2st1/junyi/output/atac0416/cicre/region_nt \
+--output output/atac0416/cicre/region_nt \
 --region AMY \
 --condition MC \
 
 python 15circepipeline.py --input output/atac0416/3REGIONS_peak.h5ads \
---output /data2st1/junyi/output/atac0416/cicre/region_nt \
+--output output/atac0416/cicre/region_nt \
 --celltype_column region_nt \
 --region PFC \
 --condition MC
 
 python 15circepipeline.py --input output/atac0416/3REGIONS_peak.h5ads \
 --celltype_column region_nt \
---output /data2st1/junyi/output/atac0416/cicre/region_nt \
+--output output/atac0416/cicre/region_nt \
 --region HIP \
 --condition MW 
 
 python 15circepipeline.py --input output/atac0416/3REGIONS_peak.h5ads \
 --celltype_column region_nt \
---output /data2st1/junyi/output/atac0416/cicre/region_nt \
+--output output/atac0416/cicre/region_nt \
 --region AMY \
 --condition MW \
 
 python 15circepipeline.py --input output/atac0416/3REGIONS_peak.h5ads \
---output /data2st1/junyi/output/atac0416/cicre/region_nt \
+--output output/atac0416/cicre/region_nt \
 --celltype_column region_nt \
 --region PFC \
 --condition MW
