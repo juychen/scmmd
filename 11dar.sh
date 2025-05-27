5ca850a48e0d41d647079b71dad4b204397b1a95
Junyi <winghare@hotmail.com>
2025-05-27 18:00:55 +0800

    Eidt dar

--- 11dar.sh
+++ 11dar.sh
@@ -6,21 +6,18 @@ conda activate snapatac2
 python 11darbysubset.py --input output/atac0416/3REGIONS_peak.h5ads \
 --output /data2st1/junyi/output/atac0526/dar \
 --celltype_column region_nt \
---method memento-ht \
---cov_column Dbatch \
+--method memento-binary \
 --region PFC
 
 python 11darbysubset.py --input output/atac0416/3REGIONS_peak.h5ads \
 --celltype_column region_nt \
 --output /data2st1/junyi/output/atac0526/dar \
---method memento-ht \
---cov_column Dbatch \
+--method memento-binary \
 --region HIP
 
 python 11darbysubset.py --input output/atac0416/3REGIONS_peak.h5ads \
 --celltype_column region_nt \
 --output /data2st1/junyi/output/atac0526/dar \
---method memento-ht \
---cov_column Dbatch \
+--method memento-binary \
 --region AMY
 
