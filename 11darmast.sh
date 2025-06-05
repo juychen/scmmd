7e0443d48b044d67ee0ba6ecbc90c29c1cf85282
Junyi <winghare@hotmail.com>
2025-06-05 18:16:41 +0800

    Edit cistopic

--- 11darmast.sh
+++ 11darmast.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 source /home/junyichen/anaconda3/etc/profile.d/conda.sh
 conda activate r_env
-Rscript 11darmast.R --input /data1st2/junyi/output/atac0416/subset/region_nt/PFC_PFC_Glut.h5ad \
+Rscript 11darmast.R --input /data1st2/junyi/output/atac0416/subset/region_nt/AMY_AMY_Glut.h5ad \
 --output /data2st1/junyi/output/atac0526/dar/region_nt/mast \
---celltype_name PFC_PFC_Glut
+--celltype_name AMY_AMY_Glut
 
-Rscript 11darmast.R --input /data1st2/junyi/output/atac0416/subset/region_nt/PFC_PFC_GABA.h5ad \
+Rscript 11darmast.R --input /data1st2/junyi/output/atac0416/subset/region_nt/AMY_AMY_GABA.h5ad \
 --output /data2st1/junyi/output/atac0526/dar/region_nt/mast \
---celltype_name PFC_PFC_GABA
+--celltype_name AMY_AMY_GABA
 
+Rscript 11darmast.R --input /data1st2/junyi/output/atac0416/subset/region_nt/HIP_HIP_GABA.h5ad \
+--output /data2st1/junyi/output/atac0526/dar/region_nt/mast \
+--celltype_name HIP_HIP_GABA
+
+Rscript 11darmast.R --input /data1st2/junyi/output/atac0416/subset/region_nt/HIP_HIP_Glut.h5ad \
+--output /data2st1/junyi/output/atac0526/dar/region_nt/mast \
+--celltype_name HIP_HIP_Glut
