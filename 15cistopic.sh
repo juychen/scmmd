588907fe1f736435108df38eacc2a65c5683479b
winghare <winghare@gmail.com>
2025-06-10 13:29:38 +0800

    edit the tespycistopic

--- 15cistopic.sh
+++ 15cistopic.sh
@@ -5,8 +5,17 @@ conda activate scenicplus
 python 15pycistopic.py --input /data1st2/junyi/output/atac0416/subset/region_nt/AMY_AMY_GABA.h5ad \
 --output /data1st2/junyi/output/atac0416/cistopic \
 
-# python 15cistopic.py --input /data1st2/junyi/output/atac0416/subset/region_nt/HIP_HIP_GABA.h5ad \
-# --output /data1st2/junyi/output/atac0416/cistopic \
+python 15pycistopic.py --input /data1st2/junyi/output/atac0416/subset/region_nt/AMY_AMY_Glut.h5ad \
+--output /data1st2/junyi/output/atac0416/cistopic \
 
-# python 15cistopic.py --input /data1st2/junyi/output/atac0416/subset/region_nt/PFC_PFC_GABA.h5ad \
-# --output /data1st2/junyi/output/atac0416/cistopic \
+python 15pycistopic.py --input /data1st2/junyi/output/atac0416/subset/region_nt/HIP_HIP_GABA.h5ad \
+--output /data1st2/junyi/output/atac0416/cistopic \
+
+python 15pycistopic.py --input /data1st2/junyi/output/atac0416/subset/region_nt/HIP_HIP_Glut.h5ad \
+--output /data1st2/junyi/output/atac0416/cistopic \
+
+python 15pycistopic.py --input /data1st2/junyi/output/atac0416/subset/region_nt/PFC_PFC_GABA.h5ad \
+--output /data1st2/junyi/output/atac0416/cistopic \
+
+python 15pycistopic.py --input /data1st2/junyi/output/atac0416/subset/region_nt/PFC_PFC_Glut.h5ad \
+--output /data1st2/junyi/output/atac0416/cistopic \
--- run_scenicp.sh
+++ run_scenicp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source /home/junyichen/anaconda3/etc/profile.d/conda.sh
 conda activate scenicplus
-cd /data1st2/junyi/output/atac0416/scenic/AMY_AMY_Glut/Snakemake
+# cd /data1st2/junyi/output/atac0416/scenic/AMY_AMY_Glut/Snakemake
+# snakemake --cores 32
+cd /data1st2/junyi/output/atac0416/scenic/HIP_HIP_Glut/Snakemake
 snakemake --cores 32
