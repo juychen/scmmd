5bb6facc08ba9294d4bb8ae7c021e8166d287713
junyichen <winghare@hotmail.com>
2025-06-13 15:10:36 +0800

    edit the scenic

--- run_scenicp.sh
+++ run_scenicp.sh
@@ -2,6 +2,17 @@
 source /home/junyichen/anaconda3/etc/profile.d/conda.sh
 conda activate scenicplus
 # cd /data1st2/junyi/output/atac0416/scenic/AMY_AMY_Glut/Snakemake
+# # snakemake --cores 32
+# cd /data1st2/junyi/output/atac0416/scenic/AMY_AMY_Glut/Snakemake
 # snakemake --cores 32
-cd /data1st2/junyi/output/atac0416/scenic/AMY_AMY_Glut/Snakemake
+
+# cd /data1st2/junyi/output/atac0416/scenic/AMY_AMY_Glut/Snakemake
+# snakemake --cores 32
+cd /data1st2/junyi/output/atac0416/scenic/PFC_PFC_GABA/Snakemake
+snakemake --cores 32
+
+cd /data1st2/junyi/output/atac0416/scenic/AMY_AMY_GABA/Snakemake
+snakemake --cores 32
+
+cd /data1st2/junyi/output/atac0416/scenic/HIP_HIP_GABA/Snakemake
 snakemake --cores 32
