adb25ab16e6a510761a2fcd1d11cec796c5b0ed1
junyichen <winghare@hotmail.com>
2025-07-02 15:55:08 +0800

    Edit the QC

---
+++ 04harmony_param.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source /home/junyichen/anaconda3/etc/profile.d/conda.sh
+conda activate allcools
+python 04harmony.py --input output/atac0627/HIP_atacsc_NN.h5ad --output output/atac0627 --nclust 5 --lamb 0.1
+python 04harmony.py --input output/atac0627/HIP_atacsc_NN.h5ad --output output/atac0627 --nclust 8 --lamb 0.1
+python 04harmony.py --input output/atac0627/HIP_atacsc_NN.h5ad --output output/atac0627 --nclust 10 --lamb 0.1
+python 04harmony.py --input output/atac0627/HIP_atacsc_NN.h5ad --output output/atac0627 --nclust 12 --lamb 0.1
+python 04harmony.py --input output/atac0627/HIP_atacsc_NN.h5ad --output output/atac0627 --nclust 15 --lamb 0.1
+
+python 04harmony.py --input output/atac0627/PFC_atacsc_NN.h5ad --output output/atac0627 --nclust 5 --lamb 0.1
+python 04harmony.py --input output/atac0627/PFC_atacsc_NN.h5ad --output output/atac0627 --nclust 8 --lamb 0.1
+python 04harmony.py --input output/atac0627/PFC_atacsc_NN.h5ad --output output/atac0627 --nclust 10 --lamb 0.1
+python 04harmony.py --input output/atac0627/PFC_atacsc_NN.h5ad --output output/atac0627 --nclust 12 --lamb 0.1
+python 04harmony.py --input output/atac0627/PFC_atacsc_NN.h5ad --output output/atac0627 --nclust 15 --lamb 0.1
+
+python 04harmony.py --input output/atac0627/AMY_atacsc_NN.h5ad --output output/atac0627 --nclust 5 --lamb 0.1
+python 04harmony.py --input output/atac0627/AMY_atacsc_NN.h5ad --output output/atac0627 --nclust 8 --lamb 0.1
+python 04harmony.py --input output/atac0627/AMY_atacsc_NN.h5ad --output output/atac0627 --nclust 10 --lamb 0.1
+python 04harmony.py --input output/atac0627/AMY_atacsc_NN.h5ad --output output/atac0627 --nclust 12 --lamb 0.1
+python 04harmony.py --input output/atac0627/AMY_atacsc_NN.h5ad --output output/atac0627 --nclust 15 --lamb 0.1
