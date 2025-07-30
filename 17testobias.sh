3b068e8237640f70154a3a71f4cbaad34c79730f
Junyi <winghare@hotmail.com>
2025-07-30 11:13:08 +0800

    Edit TF test

--- 17testobias.sh
+++ 17testobias.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-TOBIAS ATACorrect --bam /data1st2/junyi/output/atac0627/tobiasbam/MC25A_PFC/PFC_L2-3_IT_Glut_MC.bam \
---genome /data2st1/junyi/ref/GRCm38.p6.genome.fa \
---peaks /data2st1/junyi/output/atac0627/cCRE/peak.bed \
---blacklist /data2st1/junyi/ref/mm10-blacklist.v2.bed \
---outdir /data2st1/junyi/output/test \
---cores 16
+# TOBIAS ATACorrect --bam /data1st2/junyi/output/atac0627/tobiasbam/MC25A_PFC/PFC_L2-3_IT_Glut_MC.bam \
+# --genome /data2st1/junyi/ref/GRCm38.p6.genome.fa \
+# --peaks /data2st1/junyi/output/atac0627/cCRE/peak.bed \
+# --blacklist /data2st1/junyi/ref/mm10-blacklist.v2.bed \
+# --outdir /data2st1/junyi/output/test \
+# --cores 16
+
+TOBIAS FootprintScores --signal /data2st1/junyi/output/test/PFC_L2-3_IT_Glut_MC_corrected.bw \
+--regions /data2st1/junyi/output/atac0627/cCRE/peak.bed \
+--output /data2st1/junyi/output/test/PFC_L2-3_IT_Glut_MC_footprints.bw --cores 32
