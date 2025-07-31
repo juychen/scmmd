39f48536459182b8b43776bd63c84282c26b3189
Junyi <winghare@hotmail.com>
2025-07-31 10:54:25 +0800

    edit the

--- 11dar.sh
+++ 11dar.sh
@@ -3,21 +3,41 @@ source /home/junyichen/anaconda3/etc/profile.d/conda.sh
 
 conda activate snapatac2
 
-python 11darbysubset.py --input output/atac0627/3REGIONS_peak.h5ads \
---output /data2st1/junyi/output/atac0627/dar \
+# output path make if not exists
+mkdir -p /data2st1/junyi/output/atac0627/darmr
+
+python 11darbysubset.py --input /data2st1/junyi/output/atac0627/3REGIONS_dmr.h5ads \
+--output /data2st1/junyi/output/atac0627/darmr \
 --celltype_column All \
 --method memento-binary \
 --region PFC
 
-python 11darbysubset.py --input output/atac0627/3REGIONS_peak.h5ads \
+python 11darbysubset.py --input /data2st1/junyi/output/atac0627/3REGIONS_dmr.h5ads \
 --celltype_column All \
---output /data2st1/junyi/output/atac0627/dar \
+--output /data2st1/junyi/output/atac0627/darmr \
 --method memento-binary \
 --region HIP
 
-python 11darbysubset.py --input output/atac0627/3REGIONS_peak.h5ads \
+python 11darbysubset.py --input /data2st1/junyi/output/atac0627/3REGIONS_dmr.h5ads \
 --celltype_column All \
 --output /data2st1/junyi/output/atac0627/dar \
 --method memento-binary \
 --region AMY
 
+python 11darbysubset.py --input /data2st1/junyi/output/atac0627/3REGIONS_peak.h5ads \
+--output /data2st1/junyi/output/atac0627/dar \
+--celltype_column celltype.L2 \
+--method wilcoxon \
+--region PFC
+
+python 11darbysubset.py --input /data2st1/junyi/output/atac0627/3REGIONS_peak.h5ads \
+--celltype_column celltype.L2 \
+--output /data2st1/junyi/output/atac0627/dar \
+--method wilcoxon \
+--region HIP
+
+python 11darbysubset.py --input /data2st1/junyi/output/atac0627/3REGIONS_peak.h5ads \
+--celltype_column celltype.L2 \
+--output /data2st1/junyi/output/atac0627/dar \
+--method wilcoxon \
+--region AMY
