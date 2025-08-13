b9fb009414df6cb4eb7a391eff9a2cfc2383c06f
Junyi <winghare@hotmail.com>
2025-08-13 18:00:38 +0800

    eidt the analysis

--- 17GOmodulep.sh
+++ 17GOmodulep.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source /home/junyichen/anaconda3/etc/profile.d/conda.sh
 conda activate snap
-python testclf.py --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gosummary_module/1_gosummary.csv |\
-python testclf.py --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gosummary_module/2_gosummary.csv |\
-python testclf.py --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gosummary_module/3_gosummary.csv |\
-python testclf.py --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gosummary_module/4_gosummary.csv ;
-python testclf.py --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gosummary_module/5_gosummary.csv |\
-python testclf.py --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gosummary_module/6_gosummary.csv |\
-python testclf.py --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gosummary_module/7_gosummary.csv |\
-python testclf.py --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gosummary_module/8_gosummary.csv ;
+python testclf.py --column term_idname --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gostmodule_new/TFmodule1_gosummary.csv |\
+python testclf.py --column term_idname --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gostmodule_new/TFmodule2_gosummary.csv |\
+python testclf.py --column term_idname --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gostmodule_new/TFmodule3_gosummary.csv |\
+python testclf.py --column term_idname --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gostmodule_new/TFmodule4_gosummary.csv ;
+python testclf.py --column term_idname --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gostmodule_new/TFmodule5_gosummary.csv |\
+python testclf.py --column term_idname --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gostmodule_new/TFmodule6_gosummary.csv |\
+python testclf.py --column term_idname --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gostmodule_new/TFmodule7_gosummary.csv |\
+python testclf.py --column term_idname --csv_test_path /data2st1/junyi/output/atac0627/snregulation/gostmodule_new/TFmodule8_gosummary.csv ;
