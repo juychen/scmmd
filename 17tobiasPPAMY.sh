b5cd603af8f4ded1e72b218508d8d812d29b52d6
Junyi <winghare@hotmail.com>
2025-08-18 09:48:48 +0800

    Eidt the MW amy

--- 17tobiasPPAMY.sh
+++ 17tobiasPPAMY.sh
@@ -4,7 +4,7 @@ conda activate tobias_cnv
 ulimit -n 65534
 
 cd /data1st2/junyi/output/atac0627/tobiasbam
-for folder in /data1st2/junyi/output/atac0627/tobiasbam/MC*AMY*; do
+for folder in /data1st2/junyi/output/atac0627/tobiasbam/MW5*AMY*; do
   echo $folder
   sample_name=$(basename $folder)
   echo "Processing sample: $sample_name"
