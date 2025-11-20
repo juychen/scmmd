31eb4ad86046771bfe658ce183217dee2fe1a8b0
junyichen <winghare@hotmail.com>
2025-11-20 13:00:01 +0800

    eidt

--- 15cistopic.sh
+++ 15cistopic.sh
@@ -2,12 +2,12 @@
 source /home/junyichen/anaconda3/etc/profile.d/conda.sh
 conda activate scenicplus
 
-python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/AMY_AMY_GABA.h5ad --output /data2st1/junyi/output/atac1112/cistopic &\
-python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/AMY_AMY_Glut.h5ad --output /data2st1/junyi/output/atac1112/cistopic ;\
-python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/HIP_HIP_GABA.h5ad --output /data2st1/junyi/output/atac1112/cistopic &\
-python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/HIP_HIP_Glut.h5ad --output /data2st1/junyi/output/atac1112/cistopic ;\
-python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/PFC_PFC_GABA.h5ad --output /data2st1/junyi/output/atac1112/cistopic &\
-python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/PFC_PFC_Glut.h5ad --output /data2st1/junyi/output/atac1112/cistopic ;\
-python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/HIP_NN.h5ad --output /data2st1/junyi/output/atac1112/cistopic &\
-python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/AMY_NN.h5ad --output /data2st1/junyi/output/atac1112/cistopic ;\
-python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/PFC_NN.h5ad --output /data2st1/junyi/output/atac1112/cistopic \
+# python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/AMY_AMY_GABA.h5ad --output /data2st1/junyi/output/atac1112/cistopic &\
+# python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/AMY_AMY_Glut.h5ad --output /data2st1/junyi/output/atac1112/cistopic ;\
+# python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/HIP_HIP_GABA.h5ad --output /data2st1/junyi/output/atac1112/cistopic &\
+python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/HIP_HIP_Glut.h5ad --output /data2st1/junyi/output/atac1112/cistopic;
+# python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/PFC_PFC_GABA.h5ad --output /data2st1/junyi/output/atac1112/cistopic &\
+python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/PFC_PFC_Glut.h5ad --output /data2st1/junyi/output/atac1112/cistopic;
+python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/HIP_NN.h5ad --output /data2st1/junyi/output/atac1112/cistopic;
+python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/AMY_NN.h5ad --output /data2st1/junyi/output/atac1112/cistopic;
+# python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/PFC_NN.h5ad --output /data2st1/junyi/output/atac1112/cistopic \
