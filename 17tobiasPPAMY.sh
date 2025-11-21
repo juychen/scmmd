5eeb9c9b501aeda10b3d071e1c9f10d5b0f1d4b9
junyichen <winghare@hotmail.com>
2025-11-21 15:26:09 +0800

    edit cistopic

--- 17tobiasPPAMY.sh
+++ 17tobiasPPAMY.sh
@@ -4,7 +4,7 @@ conda activate tobias
 ulimit -n 65534
 
 cd /data2st2/junyi/output/atac1112/tobiasbam
-for folder in /data2st2/junyi/output/atac1112/tobiasbam/MC_AMY; do
+for folder in /data2st2/junyi/output/atac1112/tobiasbam/MW_AMY; do
   echo $folder
   sample_name=$(basename $folder)
   echo "Processing sample: $sample_name"
@@ -41,7 +41,7 @@ for folder in /data2st2/junyi/output/atac1112/tobiasbam/MC_AMY; do
           echo "Output BW file $out_fp not exists. calculating..."
           echo "generating footprints $out_fp"
           TOBIAS FootprintScores --signal $folder/corrected/$ctname\_corrected.bw \
-          --peaks /data2st1/junyi/output/atac1112/cCRE/L2peaks/${ctname:4:-3}\_peaks.bed \
+          --regions /data2st1/junyi/output/atac1112/cCRE/L2peaks/${ctname:4:-3}\_peaks.bed \
           --output $out_fp \
           --cores 64
         fi
