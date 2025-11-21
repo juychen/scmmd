004acb50448a56f67e3f28a5108004ce3dced923
junyichen <winghare@hotmail.com>
2025-11-21 11:11:35 +0800

    Add peak

--- 17tobiasPPHIP.sh
+++ 17tobiasPPHIP.sh
@@ -3,8 +3,8 @@ source /home/junyichen/anaconda3/etc/profile.d/conda.sh
 conda activate tobias
 ulimit -n 65534
 
-cd /data2st2/junyi/output/atac0627/tobiasbam
-for folder in /data2st2/junyi/output/atac0627/tobiasbam/MC_HIP; do
+cd /data2st2/junyi/output/atac1112/tobiasbam
+for folder in /data2st2/junyi/output/atac1112/tobiasbam/MC_HIP; do
   echo $folder
   sample_name=$(basename $folder)
   echo "Processing sample: $sample_name"
@@ -30,7 +30,7 @@ for folder in /data2st2/junyi/output/atac0627/tobiasbam/MC_HIP; do
           echo "Running TOBIAS ATACorrect for $out_bam"
           TOBIAS ATACorrect --bam $out_bam \
           --genome /data2st1/junyi/ref/GRCm38.p6.genome.fa \
-          --peaks /data2st1/junyi/output/atac0627/cCRE/peak.bed \
+          --peaks /data2st1/junyi/output/atac1112/cCRE/peak.bed \
           --blacklist /data2st1/junyi/ref/mm10-blacklist.v2.bed \
           --outdir $folder/corrected \
           --cores 64
@@ -41,7 +41,7 @@ for folder in /data2st2/junyi/output/atac0627/tobiasbam/MC_HIP; do
           echo "Output BW file $out_fp not exists. calculating..."
           echo "generating footprints $out_fp"
           TOBIAS FootprintScores --signal $folder/corrected/$ctname\_corrected.bw \
-          --regions /data2st1/junyi/output/atac0627/cCRE/peak.bed \
+          --regions /data2st1/junyi/output/atac1112/cCRE/peak.bed \
           --output $out_fp \
           --cores 64
         fi
@@ -52,13 +52,13 @@ for folder in /data2st2/junyi/output/atac0627/tobiasbam/MC_HIP; do
       # Uncomment the line below if you want to run the command for each file
     #   /home/junyichen/subset-bam_linux --bam /data1st2/hannan_25/data/snATAC_process/snATAC_01_bam/$sample_name/outs/possorted_bam.bam \
     #   --cell-barcodes $file \
-    #   --out-bam /data1st2/junyi/output/atac0627/tobiasbam/$celltype/${file%.csv}.bam \
+    #   --out-bam /data1st2/junyi/output/atac1112/tobiasbam/$celltype/${file%.csv}.bam \
     #   --cores 32
     fi
   done
 done
 
 # /home/junyichen/subset-bam_linux --bam /data1st2/hannan_25/data/snATAC_process/snATAC_01_bam/MC25A_PFC/outs/possorted_bam.bam \
-# --cell-barcodes /data1st2/junyi/output/atac0627/tobiasbam/MC25A_PFC/PFC_L2-3_IT_Glut_MC.csv \
-# --out-bam /data1st2/junyi/output/atac0627/tobiasbam/MC25A_PFC/PFC_L2-3_IT_Glut_MC.bam \
+# --cell-barcodes /data1st2/junyi/output/atac1112/tobiasbam/MC25A_PFC/PFC_L2-3_IT_Glut_MC.csv \
+# --out-bam /data1st2/junyi/output/atac1112/tobiasbam/MC25A_PFC/PFC_L2-3_IT_Glut_MC.bam \
 # --cores 32
