3f4d85e8ab899ed6c2b8cfb29bbb5e25cc224d6d
junyichen <winghare@hotmail.com>
2025-11-25 10:26:19 +0800

    eidt thi summary

--- 15cistopic.sh
+++ 15cistopic.sh
@@ -7,7 +7,7 @@ conda activate scenicplus
 # python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/HIP_HIP_GABA.h5ad --output /data2st1/junyi/output/atac1112/cistopic &\
 # python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/HIP_HIP_Glut.h5ad --output /data2st1/junyi/output/atac1112/cistopic;
 # python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/PFC_PFC_GABA.h5ad --output /data2st1/junyi/output/atac1112/cistopic &\
-python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/PFC_PFC_Glut.h5ad --output /data2st1/junyi/output/atac1112/cistopic;
-# python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/HIP_NN.h5ad --output /data2st1/junyi/output/atac1112/cistopic;
-# python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/AMY_NN.h5ad --output /data2st1/junyi/output/atac1112/cistopic;
+# python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/PFC_PFC_Glut.h5ad --output /data2st1/junyi/output/atac1112/cistopic;
+python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/HIP_NN.h5ad --output /data2st1/junyi/output/atac1112/cistopic;
+python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/AMY_NN.h5ad --output /data2st1/junyi/output/atac1112/cistopic;
 # python 15pycistopic.py --input /data2st1/junyi/output/atac1112/subset/region_nt/PFC_NN.h5ad --output /data2st1/junyi/output/atac1112/cistopic \
