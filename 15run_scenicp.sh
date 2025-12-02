b83a99d685517b3f6283a0104b7c794b4125e631
junyichen <winghare@hotmail.com>
2025-12-02 22:00:31 +0800

    Eidt

--- 15run_scenicp.sh
+++ 15run_scenicp.sh
@@ -3,19 +3,19 @@ source /home/junyichen/anaconda3/etc/profile.d/conda.sh
 conda activate scenicplus
 cd /data2st1/junyi/output/atac1112/scenic/AMY_AMY_Glut/Snakemake
 snakemake --cores 32
-# cd /data2st1/junyi/output/atac1112/scenic/PFC_PFC_Glut/Snakemake
-# snakemake --cores 32
-# cd /data2st1/junyi/output/atac1112/scenic/HIP_HIP_Glut/Snakemake
-# snakemake --cores 32
-# cd /data2st1/junyi/output/atac1112/scenic/PFC_PFC_GABA/Snakemake
-# snakemake --cores 32
-# cd /data2st1/junyi/output/atac1112/scenic/AMY_AMY_GABA/Snakemake
-# snakemake --cores 32
-cd /data2st1/junyi/output/atac1112/scenic/HIP_HIP_GABA/Snakemake
+cd /data2st1/junyi/output/atac1112/scenic/PFC_PFC_Glut/Snakemake
 snakemake --cores 32
-cd /data2st1/junyi/output/atac1112/scenic/PFC_NN/Snakemake
+cd /data2st1/junyi/output/atac1112/scenic/HIP_HIP_Glut/Snakemake
 snakemake --cores 32
-cd /data2st1/junyi/output/atac1112/scenic/AMY_NN/Snakemake
+cd /data2st1/junyi/output/atac1112/scenic/PFC_PFC_GABA/Snakemake
 snakemake --cores 32
-cd /data2st1/junyi/output/atac1112/scenic/HIP_NN/Snakemake
+cd /data2st1/junyi/output/atac1112/scenic/AMY_AMY_GABA/Snakemake
 snakemake --cores 32
+# cd /data2st1/junyi/output/atac1112/scenic/HIP_HIP_GABA/Snakemake
+# snakemake --cores 32
+# cd /data2st1/junyi/output/atac1112/scenic/PFC_NN/Snakemake
+# snakemake --cores 32
+# cd /data2st1/junyi/output/atac1112/scenic/AMY_NN/Snakemake
+# snakemake --cores 32
+# cd /data2st1/junyi/output/atac1112/scenic/HIP_NN/Snakemake
+# snakemake --cores 32
