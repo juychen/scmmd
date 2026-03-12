a5db4d75f171090c08534a0780b7953bc29b0669
junyichen <winghare@hotmail.com>
2026-03-12 16:19:12 +0800

    edit

--- 11darmast_dmr.sh
+++ 11darmast_dmr.sh
@@ -13,4 +13,4 @@ conda activate r_env
 # Rscript 11darmast_new.R --input /data1st2/junyi/output/atac1112/subset/region_nt/AMY_AMY_GABA.h5ad \
 # --output /data2st1/junyi/output/atac1112/dar/region/MAST \
 mkdir -p /data2st1/junyi/output/atac1112/dar/region/MASTdmr  
-Rscript 11darmast_dmr.R --input /data2st1/junyi/output/atac1112/subset/dmr_region_nt/PFC_5hmC.h5ad --output /data2st1/junyi/output/atac1112/dar/region/MASTdmr --region "PFC" &\
+Rscript 11darmast_new_dmr.R --input /data2st1/junyi/output/atac1112/subset/dmr_region_nt/PFC_5hmC.h5ad --output /data2st1/junyi/output/atac1112/dar/region/MASTdmr --region "PFC" &\
