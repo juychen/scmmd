459469a9b97fe0faacaa16819a7a6aad9eb9f7e5
junyichen <winghare@hotmail.com>
2026-03-12 16:44:28 +0800

    edith mast

--- 11darmast_dmr.sh
+++ 11darmast_dmr.sh
@@ -12,5 +12,11 @@ conda activate r_env
 
 # Rscript 11darmast_new.R --input /data1st2/junyi/output/atac1112/subset/region_nt/AMY_AMY_GABA.h5ad \
 # --output /data2st1/junyi/output/atac1112/dar/region/MAST \
-mkdir -p /data2st1/junyi/output/atac1112/dar/region/MASTdmr  
-Rscript 11darmast_new_dmr.R --input /data2st1/junyi/output/atac1112/subset/dmr_region_nt/PFC_5hmC.h5ad --output /data2st1/junyi/output/atac1112/dar/region/MASTdmr --region "PFC" &\
+mkdir -p /data2st1/junyi/output/atac1112/dar/region/MASTdmr5mC
+mkdir -p /data2st1/junyi/output/atac1112/dar/region/MASTdmr5hmC  
+Rscript 11darmast_new_dmr.R --input /data2st1/junyi/output/atac1112/subset/dmr_region_nt/PFC_5mC.h5ad --output /data2st1/junyi/output/atac1112/dar/region/MASTdmr5mC --region "PFC" &\
+Rscript 11darmast_new_dmr.R --input /data2st1/junyi/output/atac1112/subset/dmr_region_nt/PFC_5hmC.h5ad --output /data2st1/junyi/output/atac1112/dar/region/MASTdmr5hmC --region "PFC";
+Rscript 11darmast_new_dmr.R --input /data2st1/junyi/output/atac1112/subset/dmr_region_nt/HIP_5mC.h5ad --output /data2st1/junyi/output/atac1112/dar/region/MASTdmr5mC --region "HIP" &\
+Rscript 11darmast_new_dmr.R --input /data2st1/junyi/output/atac1112/subset/dmr_region_nt/HIP_5hmC.h5ad --output /data2st1/junyi/output/atac1112/dar/region/MASTdmr5hmC --region "HIP" &\
+Rscript 11darmast_new_dmr.R --input /data2st1/junyi/output/atac1112/subset/dmr_region_nt/AMY_5mC.h5ad --output /data2st1/junyi/output/atac1112/dar/region/MASTdmr5mC --region "AMY" &\
+Rscript 11darmast_new_dmr.R --input /data2st1/junyi/output/atac1112/subset/dmr_region_nt/AMY_5hmC.h5ad --output /data2st1/junyi/output/atac1112/dar/region/MASTdmr5hmC --region "AMY";
