af644de72c51312871614e0c99d46fd52f93ba2f
junyichen <winghare@hotmail.com>
2026-05-06 16:45:52 +0800

    Edit

--- 10_peak_genecorr.sh
+++ 10_peak_genecorr.sh
@@ -11,8 +11,8 @@ source /home/junyichen/anaconda3/etc/profile.d/conda.sh
 conda activate snapatac2
 cd /home/junyichen/code/scmmd/
 python peak_gene_corr_pseudobulk.py \
-  --adata-bulk /path/to/adata_3REGION_bulk.h5ad \
-  --adata-atac /path/to/adata_3REGION_atac.h5ad \
+  --adata-bulk /data2st2/junyi/output/stg1028/CUMS_4VN/CUMS_4VN_bulk.h5ad \
+  --adata-atac /data2st1/junyi/output/atac1112/3REGIONS_peak_l2_aggregated.h5ad \
   --promoter-peak /data2st1/junyi/output/atac1112/3REGIONS_peak_l2_promoter_peak_500kb.csv \
   --out-prefix /data2st1/junyi/output/atac1112/cCRE \
   --chunks 40
